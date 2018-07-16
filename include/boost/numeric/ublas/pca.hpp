205921d421660674d945f216c2524f72ee509d50
dattatreya303 <chikumhptr@gmail.com>
2018-07-17 01:46:12 +0530

    Add basic prototypes for PCA class and methods.

---
+++ include/boost/numeric/ublas/pca.hpp
@@ -0,0 +1,88 @@
+#include <boost/numeric/ublas/matrix.hpp>
+#include <boost/numeric/ublas/matrix_proxy.hpp>
+#include <boost/numeric/ublas/vector.hpp>
+#include <boost/numeric/ublas/vector_proxy.hpp>
+
+namespace boost { namespace numeric { namespace ublas {
+
+    class PCA {
+    public:
+        PCA () {}
+
+        template <class MatrixType>
+        double Apply (const MatrixType &data,
+                      matrix<double> &transformed_data,
+                      const int n_components) {
+            
+            return Apply (data, transformed_data, (size_t)n_components);
+        }
+
+        template <class MatrixType>
+        double Apply (const MatrixType &data,
+                      matrix<double> &transformed_data,
+                      const size_t n_components) {
+
+            assert (n_components > 0);
+            assert (n_components <= data.size2 ());
+
+            matrix<double> transformed_data_temp (data.size1 (), data.size2 ());
+            vector<double> eigen_values (data.size2 ());
+            matrix<double> eigen_vectors (data.size2 (), data.size2 ());
+            
+            Apply (data, transformed_data_temp, eigen_values, eigen_vectors);
+            
+            transformed_data = subrange (transformed_data_temp, 0, data.size1 (), 0, n_components);
+        }
+
+        template <class MatrixType>
+        void Apply (const MatrixType &data,
+                    matrix<double> &transformed_data,
+                    const double variance_retained) {
+
+            matrix<double> transformed_data_temp (data.size1 (), data.size2 ());
+            vector<double> eigen_values (data.size2 ());
+            matrix<double> eigen_vectors (data.size2 (), data.size2 ());
+            
+            Apply (data, transformed_data_temp, eigen_values, eigen_vectors);
+            
+            size_t eig_counter = 0;
+            double cur_eig_val_sum = 0;
+            double total_eig_val_sum = sum (eigen_values);
+            while (eig_counter < eigen_values.size ()) {
+                cur_eig_val_sum += eigen_values (eig_counter);
+                double cumulative_variance = cur_eig_val_sum / total_eig_val_sum;
+                if (cumulative_variance >= variance_retained)
+                    break;
+                ++ eig_counter;
+            }
+
+            transformed_data = subrange (transformed_data_temp, 0, data.size1 (), 0, eig_counter + 1);
+        }
+
+        template <class MatrixType>
+        void Apply (const MatrixType &data,
+                    matrix<double> &transformed_data,
+                    vector<double> eigen_values,
+                    matrix<double> eigen_vectors) {
+
+            matrix<double> centered_data (data.size1 (), data.size2 ());
+            CenterData (data, centered_data);
+
+            size_t n = data.size1 ();
+            MatrixType empirical_covariance_matrix = (1 / (n - 1)) * (trans (data) * data);
+
+            // Code to get eigen values and eigen vectors.
+
+            transformed_data = centered_data * eigen_vectors;
+        }
+
+    private:
+
+        template <class MatrixType>
+        void CenterData (const MatrixType &data, matrix<double> &centered_data) {
+            centered_data = trans (scalar_vector<int> (1)) * mean (data, 0);
+        }
+
+    };
+
+}}}
